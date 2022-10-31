b866062b6eeaafd98f3f1dc5f791071a726b9873
pahariyatri <pahariyatri@gmail.com>
2022-10-31 13:03:29 +0530

    changed greeting

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express= require('express');
 const app= express();
 
 app.get('/',(req,res)=>{
-    res.send({hi : 'there'})
+    res.send({hey : 'the fuck you want here'})
 });
+
 
 const PORT= process.env.PORT || 3000
 app.listen(PORT);
