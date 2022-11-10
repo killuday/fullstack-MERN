63ba97c6f33279e6488bf04f34c5ded57d7ced5d
pahariyatri <bhanusharm122@gmail.com>
2022-11-10 15:21:12 +0530

    fixing versions

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,12 @@ import reduxThunk from 'redux-thunk';
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 
-ReactDom.render(<Provider store={store}><App /></Provider>, 
-document.querySelector('#root'));
+//ReactDom.render(<Provider store={store}><App /></Provider>, 
+//document.querySelector('#root'));
+
+const root=ReactDom.createRoot(document.getElementById('root'));
+root.render(<Provider store={store}><App /></Provider>);
+
 //!console.log("STRIPE KEY IS ",process.env.REACT_APP_STRIPE_KEY)
 //!console.log("Environment is ",process.env.NODE_ENV);
 
